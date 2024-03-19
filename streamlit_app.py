2388f15db54bd8d7f14a7aba6868846fc8cf9fb6
gamechanger2115 <160454876+gamechanger2115@users.noreply.github.com>
2024-03-19 14:15:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,10 +49,11 @@ if ingredients_list:
     #st.write(my_insert_stmt)
     #st.stop()
     time_to_insert =  st.button('Submit Order')
-    if ingredients_string:
+ 
         session.sql(my_insert_stmt).collect()
     st.success(' Your Smoothie is ordered!', icon='✅')
 #st.text(fruityvice_response.json())
+
 
 
 
