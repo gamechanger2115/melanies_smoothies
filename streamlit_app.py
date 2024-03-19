3504aa0fcd595e8427d43e72228da122c085c0f4
gamechanger2115 <160454876+gamechanger2115@users.noreply.github.com>
2024-03-19 14:56:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,14 +46,12 @@ if ingredients_list:
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" +name_on_order+ """')"""
  
-    #st.write(my_insert_stmt)
+     #st.write(my_insert_stmt)
     #st.stop()
     time_to_insert =  st.button('Submit Order')
-    if ingredients_string:
-        session.sql(my_insert_stmt).collect()
-    st.success(' Your Smoothie is ordered!', icon='✅')
-#st.text(fruityvice_response.json())
-
+    if time_to_insert:
+       session.sql(my_insert_stmt).collect()
+    st.success(' Your Smoothie is Ordered !', icon='✅')
 
 
 
